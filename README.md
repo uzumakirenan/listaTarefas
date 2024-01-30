bc6e322ec3ffbd92152ddbfcbf2b9839e241f1dd
renancano <uzumaki.renandroid@gmail.com>
2024-01-29 21:03:10 -0300

    continue

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ListaTarefas
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1. RENAN
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1.
 
 ## Development server
 
--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Output, ViewChild, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, ViewChild, inject } from '@angular/core';
 import { IListItems } from '../../interfaces/IListItems.interface';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
@@ -13,6 +14,7 @@ export class InputAddItemComponent {
 
   @ViewChild("inputText") public inputText!: ElementRef
 
+  @Input({ required: true}) public inputListItems:IListItems[] = []
   @Output() public outputAddListItem = new EventEmitter<IListItems>()
 
   public focusAndAddItem(value: string) {
