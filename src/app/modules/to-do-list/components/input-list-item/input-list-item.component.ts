39ac84b3c8f70a0184d8b8458416796f24d6f39e
renancano <uzumaki.renandroid@gmail.com>
2024-02-03 16:44:24 -0300

    Finalização do projeto

---
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { IListItems } from '../../interfaces/IListItems.interface';
+
+@Component({
+  selector: 'app-input-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-list-item.component.html',
+  styleUrl: './input-list-item.component.scss'
+})
+export class InputListItemComponent {
+
+  @Input({ required: true }) public inputListItems:IListItems[] = []
+  
+  @Output() public outputUpdateItemCheckbox = new EventEmitter<{ id:string, checked:boolean }> 
+
+  public updateItemCheckbox(id:string, checked:boolean){
+    return this.outputUpdateItemCheckbox.emit({id, checked})
+  }
+
+  @Output() public outputUpdateItemText = new EventEmitter<{ id:string, value:string }> 
+
+  public updateItemText(id:string, value:string){
+    return this.outputUpdateItemText.emit({id, value})
+  }
+
+  @Output() public outputDeleteItem = new EventEmitter<string> 
+
+  public deleteItem(id:string){
+    return this.outputDeleteItem.emit(id)
+  }
+}
--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 import { IListItems } from '../../interfaces/IListItems.interface';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -20,6 +22,10 @@ export class ListComponent {
     return JSON.parse(localStorage.getItem("@my-list") || "[]")
   }
 
+  #updateLocalStorage(){
+    return localStorage.setItem("@my-list", JSON.stringify(this.#setListItems()))
+  }
+
   public getInputAndAddItem(value: IListItems) {
     localStorage.setItem(
       '@my-list', JSON.stringify([...this.#setListItems(), value])
@@ -28,8 +34,86 @@ export class ListComponent {
     return this.#setListItems.set(this.#parseItems())
   }
 
+  public listItemsStage(value: 'pending' | 'completed'){
+    return this.getListItems().filter((res: IListItems) => {
+      if(value === "pending"){
+        return !res.checked
+      }
+
+      if(value === "completed"){
+        return res.checked
+      }
+
+      return res
+    })
+  }
+
+  public updateItemCheckbox(newItem:{id:string, checked:boolean}){
+    this.#setListItems.update((oldValue:IListItems[]) => {
+      oldValue.filter( res => {
+        if(res.id === newItem.id){
+          res.checked = newItem.checked
+          return res
+        }
+
+        return res
+      })
+
+      return oldValue
+    })
+
+    return this.#updateLocalStorage()
+  }
+
+  public updateItemText(newItem:{id:string, value:string}){
+    this.#setListItems.update((oldValue:IListItems[]) => {
+      oldValue.filter( res => {
+        if(res.id === newItem.id){
+          res.value = newItem.value
+          return res
+        }
+
+        return res
+      })
+
+      return oldValue
+    })
+
+    return this.#updateLocalStorage()
+  }
+
+  public deleteItem(id:string){
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Você não poderá reverter isso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sim, delete este item",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue:IListItems[]) => {
+          return oldValue.filter( res => res.id !== id)
+        })
+    
+        return this.#updateLocalStorage()
+      }
+    });
+  }
+
   public deleteAllItems(){
-    localStorage.removeItem("@my-list")
-    return this.#setListItems.set(this.#parseItems())
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Você não poderá reverter isso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sim, deletar tudo!",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("@my-list")
+        return this.#setListItems.set(this.#parseItems())
+      }
+    });
   }
 }
